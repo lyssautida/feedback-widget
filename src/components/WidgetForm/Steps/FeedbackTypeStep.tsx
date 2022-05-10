b3d41f3ea221d68edb2d4f1d7d322943f35300b0
Lyssa Utida <lyssamailbox@gmail.com>
2022-05-10 15:52:16 -0400

    creating each feedback widget step: type, content and success one

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,34 +1,36 @@
-
-import { FeedbackType, feedbackTypes } from "../..";
-import { CloseButton } from "../../CloseButton"
+import { FeedbackType, feedbackTypes } from '../..'
+import { CloseButton } from '../../CloseButton'
 
 interface FeedbackTypesStepProps {
   onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
 
-export function FeedbackTypeStep({ onFeedbackTypeChanged}: FeedbackTypesStepProps) {
+export function FeedbackTypeStep (props
+: FeedbackTypesStepProps) {
   return (
     <>
       <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
+        <span className='text-xl leading-6'>Deixe seu feedback</span>
 
         <CloseButton />
       </header>
-      <div className="flex py-8 gap-2 w-full">
-      { Object.entries(feedbackTypes).map(([key, value])=> {
-        return (
+      <div className='flex py-8 gap-2 w-full'>
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
             <button
-                key={key}
-                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus-outline-none"
-                onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
-                //função dentro de onClink, key é obrigatoriamente os tipos de feedback ... passar uma arrow function
-                type="button"
+              key={key}
+              className='bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus-outline-none'
+              onClick={() => props.onFeedbackTypeChanged(key as FeedbackType)}
+              // função dentro de onClink, key é obrigatoriamente os tipos de feedback ... deve passar uma arrow function
+              type='button'
             >
+              <span>
                 <img src={value.image.source} alt={value.image.alt} />
-                <span>{value.title}</span>
+                {value.title}
+              </span>
             </button>
           )
-      }) }
+        })}
       </div>
     </>
   )
